8eb0cbe3c0514129b36460e7a2d476c2195c45e6
MuliJob <munyoki912@gmail.com>
2025-03-29 22:10:51 +0300

    Adding tailwindcss

--- auto_frontend/src/views/HomeView.vue
+++ auto_frontend/src/views/HomeView.vue
@@ -3,7 +3,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class="text-6xl font-bold underline text-emerald-500">
+    Hello world!
+  </h1>
 </template>
