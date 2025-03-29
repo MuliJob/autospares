03da336b5721301c42cab7e207a95c77d5c6ccdb
MuliJob <munyoki912@gmail.com>
2025-03-29 21:32:51 +0300

    created django project

---
+++ auto_backend/auto_backend/urls.py
@@ -0,0 +1,11 @@
+"""
+URL configuration for auto_backend project.
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('djoser.urls')),
+    path('api/v1/', include('djoser.urls.authtoken')),
+]
