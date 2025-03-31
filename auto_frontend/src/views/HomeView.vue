49ba2ebcfc4ea286e3e108b9465a1a02048f37ec
MuliJob <munyoki912@gmail.com>
2025-03-31 19:03:45 +0300

    removed unnecessary pages

--- auto_frontend/src/views/HomeView.vue
+++ auto_frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <h1 class="text-6xl font-bold underline text-emerald-500">
+  <h1 class="text-6xl text-center mt-5 font-bold underline text-emerald-500">
     Hello world!
   </h1>
 </template>
